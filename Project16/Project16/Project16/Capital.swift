49afb0ff32fbb445254108e1d15147dede6670e5
matthewzierl <matthewtzierl@gmail.com>
2024-08-09 11:51:37 +0900

    Callout Accessory Button to Wiki
    Instead of showing an alert controller, pressing
    the button will now pull up the wiki page for that
    capital

--- Project16/Project16/Project16/Capital.swift
+++ Project16/Project16/Project16/Capital.swift
@@ -12,10 +12,12 @@ class Capital: NSObject, MKAnnotation {
     var title: String?
     var coordinate: CLLocationCoordinate2D // must have coordinate to conform to MKAnnotation
     var extraInfo: String
+    var wiki: String
     
-    init(title: String, coordinate: CLLocationCoordinate2D, extraInfo: String) {
+    init(title: String, coordinate: CLLocationCoordinate2D, extraInfo: String, wiki: String) {
         self.title = title
         self.coordinate = coordinate
         self.extraInfo = extraInfo
+        self.wiki = wiki
     }
 }
--- Project16/Project16/Project16/ViewController.swift
+++ Project16/Project16/Project16/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import WebKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
 
@@ -18,17 +19,17 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: SFSymbols.map), style: .plain, target: self, action: #selector(changeMapType))
         
-        let tokyo = Capital(title: "Tokyo", coordinate: CLLocationCoordinate2D(latitude: 35.652832, longitude: 139.839478), extraInfo: "My home")
+        let tokyo = Capital(title: "Tokyo", coordinate: CLLocationCoordinate2D(latitude: 35.652832, longitude: 139.839478), extraInfo: "My home", wiki: "https://en.wikipedia.org/wiki/Tokyo")
         
-        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), extraInfo: "Home to 2012 Summer Olympics")
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), extraInfo: "Home to 2012 Summer Olympics", wiki: "https://en.wikipedia.org/wiki/London")
         
-        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), extraInfo: "Founded over 100 years ago")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), extraInfo: "Founded over 100 years ago", wiki: "https://en.wikipedia.org/wiki/Oslo")
         
-        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), extraInfo: "Often called city of light")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), extraInfo: "Often called city of light", wiki: "https://en.wikipedia.org/wiki/Paris")
         
-        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), extraInfo: "Has a whole country inside it")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), extraInfo: "Has a whole country inside it", wiki: "https://en.wikipedia.org/wiki/Rome")
         
-        let washington = Capital(title: "Washington D.C", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), extraInfo: "Named after George himself")
+        let washington = Capital(title: "Washington D.C", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), extraInfo: "Named after George himself", wiki: "https://en.wikipedia.org/wiki/Washington,_D.C.")
         
         mapView.addAnnotations([tokyo, london, oslo, paris, rome, washington])
     }
@@ -77,13 +78,31 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         guard let capital = view.annotation as? Capital else { return }
         
-        let placeName = capital.title
-        let placeInfo =  capital.extraInfo
+        let vc = UIViewController()
         
-        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Okay", style: .default))
         
-        present(ac, animated: true)
+        let webView = WKWebView()
+        
+        guard let url = URL(string: capital.wiki) else { return }
+        
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        vc.view = webView
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
+        
+        
+        
+//        let placeName = capital.title
+//        let placeInfo =  capital.extraInfo
+        
+//        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "Okay", style: .default))
+//        
+//        present(ac, animated: true)
     }
 
 

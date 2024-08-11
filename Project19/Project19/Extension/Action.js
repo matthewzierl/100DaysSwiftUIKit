37566ccdb8b154d71de8b7ad5f9a4dbbaa447351
matthewzierl <matthewtzierl@gmail.com>
2024-08-12 01:42:06 +0900

    Finish Safari Browser Extension (p19)
    I really didn't understand this project... I know
    out app can't directly communicate with safari,
    so we have to use the OS as a medium. This
    involves using dictionaries to bridge between
    our app and javascript for safari.

--- Project19/Project19/Extension/Action.js
+++ Project19/Project19/Extension/Action.js
@@ -1,16 +1,18 @@
 var Action = function() { }
 
 Action.prototype = {
+
+    run: function(parameters) {
+        parameters.completionFunction({"URL": document.URL, "title": document.title});
+    },
     
-run: function(parameters) {
-    
-},
-    
-finalize: function(parameters) {
-    
-}
-    
+    finalize: function(parameters) {
+        // Add any finalization code here if needed
+        var customJavascript = parameters["customJavascript"];
+        eval(customJavascript)
+    }
     
 };
 
 var ExtensionPreprocessingJS = new Action
+
--- Project19/Project19/Extension/ActionViewController.swift
+++ Project19/Project19/Extension/ActionViewController.swift
@@ -11,10 +11,21 @@ import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var script: UITextView!
+    
+    var pageTitle: String = ""
+    var pageURL: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        let notificationCenter = NotificationCenter.default
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
     
         // Get the item[s] we're handling from the extension context.
         
@@ -30,17 +41,49 @@ class ActionViewController: UIViewController {
             
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: UTType.propertyList.identifier) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
                     
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async { // closure inside closure, will use same capture list
+                        self?.title = self?.pageTitle
+                    }
                     
                 }
             }
         }
     }
 
+    
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavascript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavascript = NSItemProvider(item: webDictionary, typeIdentifier: UTType.propertyList.identifier)
+        item.attachments = [customJavascript]
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    // a Notification contains name of notification and dictionary
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        script.verticalScrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        
+        script.scrollRangeToVisible(selectedRange)
+        
     }
 
 }

798b4b8d912b50fd40808c4bfbe8af2348fc982a
matthewzierl <matthewtzierl@gmail.com>
2024-08-12 13:04:26 +0900

    Add Progress Save, Custom Scripts, Input Scripts
    As part of the challenges, I used user defaults
    to store progress on scripts users were making
    on websites, as well as any custom scripts the
    user created, named, and stored for later use.

--- Project19/Project19/Extension/ActionViewController.swift
+++ Project19/Project19/Extension/ActionViewController.swift
@@ -9,22 +9,47 @@ import UIKit
 import MobileCoreServices
 import UniformTypeIdentifiers
 
-class ActionViewController: UIViewController {
+class ActionViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet var script: UITextView!
     
     var pageTitle: String = ""
     var pageURL: String = ""
+    
+    let upperCaseScript = "document.body.style.textTransform = \"uppercase\";"
+    let highlightLinksScript = "var links = document.querySelectorAll('a'); links.forEach(function(link) { link.style.backgroundColor = 'yellow'; });"
+    let currentURLScript = "alert(\"Current URL: \" + window.location.href);"
+    let backgroundColorScript = "document.body.style.backgroundColor = \"lightblue\";"
+    let hideImagesScript = "document.querySelectorAll('img'); images.forEach(function(img) { img.remove(); });"
+    
+    var savedProgress = [String: String]()
+    var typingTimer: Timer?
+    
+    var savedScripts = [String: String]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        script.delegate = self
+        
+        let save = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveScript))
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(listScripts))
+        
+        navigationItem.rightBarButtonItems = [done, save]
         
         let notificationCenter = NotificationCenter.default
         
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        let defaults = UserDefaults.standard
+        
+        savedProgress = defaults.object(forKey: "savedProgress") as? [String: String] ?? [String: String]()
+        
+        savedScripts = defaults.object(forKey: "savedScripts") as? [String: String] ?? [String: String]()
         
     
         // Get the item[s] we're handling from the extension context.
@@ -50,21 +75,69 @@ class ActionViewController: UIViewController {
                     
                     DispatchQueue.main.async { // closure inside closure, will use same capture list
                         self?.title = self?.pageTitle
+                        self?.script.text = self?.savedProgress[self!.pageURL]
                     }
                     
                 }
             }
         }
     }
+    
+    @objc func saveScript(_ sender: UIBarButtonItem) {
+        
+        let ac = UIAlertController(title: "Save Custom Script", message: "Please provide a name", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak self] action in
+            if let input = ac.textFields?[0].text {
+                if input == "" { return } // don't save if they didn't provide a name
+                self?.savedScripts[input] = self?.script.text
+                
+                let userDefault = UserDefaults.standard
+                userDefault.set(self?.savedScripts, forKey: "savedScripts")
+            }
+        }))
+        present(ac, animated: true)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        typingTimer?.invalidate()
+        typingTimer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(saveProgress), userInfo: nil, repeats: false)
+    }
+    
+    @objc func listScripts(_ sender: UIBarButtonItem) {
+        let ac = UIAlertController(title: "Scripts", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Upper Case", style: .default, handler: { [weak self] action in
+            self?.script.text = self?.upperCaseScript
+        }))
+        ac.addAction(UIAlertAction(title: "Highlight Links", style: .default, handler: { [weak self] action in
+            self?.script.text = self?.highlightLinksScript
+        }))
+        ac.addAction(UIAlertAction(title: "Current URL", style: .default, handler: { [weak self] action in
+            self?.script.text = self?.currentURLScript
+        }))
+        ac.addAction(UIAlertAction(title: "Change Background Color", style: .default, handler: { [weak self] action in
+            self?.script.text = self?.backgroundColorScript
+        }))
+        ac.addAction(UIAlertAction(title: "Hide Images", style: .default, handler: { [weak self] action in
+            self?.script.text = self?.hideImagesScript
+        }))
+        for (key, value) in savedScripts {
+            ac.addAction(UIAlertAction(title: key, style: .default, handler: { [weak self] action in
+                self?.script.text = value
+            }))
+        }
+        present(ac, animated: true)
+    }
 
     
     @IBAction func done() {
         let item = NSExtensionItem()
-        let argument: NSDictionary = ["customJavascript": script.text]
-        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
-        let customJavascript = NSItemProvider(item: webDictionary, typeIdentifier: UTType.propertyList.identifier)
+        
+        let argument: NSDictionary = ["customJavascript": script.text] // Make dictionary with 'customJavascript' as key and text as value
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument] // Create another dictionary with javascriptFinalize as key, and previous dictionary as value
+        let customJavascript = NSItemProvider(item: webDictionary, typeIdentifier: UTType.propertyList.identifier) // convert that dictionary to NSItemProvider so to convey data between host app and extension
         item.attachments = [customJavascript]
-        extensionContext?.completeRequest(returningItems: [item])
+        extensionContext?.completeRequest(returningItems: [item]) // completes request and returns to host app
     }
     
     // a Notification contains name of notification and dictionary
@@ -84,6 +157,13 @@ class ActionViewController: UIViewController {
         
         script.scrollRangeToVisible(selectedRange)
         
+    }
+    
+    @objc func saveProgress() {
+        print("User stopped typing for 3 seconds")
+        let defaults = UserDefaults.standard
+        savedProgress[pageURL] = script.text ?? ""
+        defaults.setValue(savedProgress, forKey: "savedProgress")
     }
 
 }

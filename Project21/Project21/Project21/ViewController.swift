28496fe90fbc999eb8816be8adea3c602be53a19
matthewzierl <matthewtzierl@gmail.com>
2024-08-13 23:35:34 +0900

    update userNotificationCenter didReceive
    Added 2 new actions: destruction and remind later
    Once app receives either action identifier, it
    shows different alert controllers, and with
    remindLater, it schedules another notification in
    10 seconds

--- Project21/Project21/Project21/ViewController.swift
+++ Project21/Project21/Project21/ViewController.swift
@@ -54,6 +54,24 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         center.add(request)
     }
     
+    func scheduleNotificatonInterval(title: String, body: String, identifier: String) {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.categoryIdentifier = identifier
+        content.userInfo = ["yes": "sir"]
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request)
+    }
+    
     func registerCategories() {
         let center = UNUserNotificationCenter.current()
         center.delegate = self
@@ -64,12 +82,13 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             options: any special actions related to the action
          */
         let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
-        let other = UNNotificationAction(identifier: "other", title: "destruction", options: .destructive)
+        let other = UNNotificationAction(identifier: "destruction", title: "Destruction", options: .destructive)
+        let remindLater = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: .foreground)
         
         /*
             identifier: must match same category identifier that we made above
          */
-        let category = UNNotificationCategory(identifier: "alarm", actions: [show, other], intentIdentifiers: [], options: [])
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, other, remindLater], intentIdentifiers: [], options: [])
         
         center.setNotificationCategories([category])
     }
@@ -81,13 +100,32 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         if let customData = userInfo["customData"] as? String {
             print("Custom Data received: \(customData)")
             
+            let ac: UIAlertController?
+            
             switch response.actionIdentifier {
             case UNNotificationDefaultActionIdentifier:
                 print("Default identifier")
+                ac = UIAlertController(title: "Lame", message: "Please use the action I gave you", preferredStyle: .alert)
+                ac?.addAction(UIAlertAction(title: "okay", style: .default))
             case "show":
                 print("User tapped OUR show more information...")
+                ac = UIAlertController(title: "YES", message: "YOU USED OUR ACTION", preferredStyle: .alert)
+                ac?.addAction(UIAlertAction(title: "COOL", style: .default))
+            case "destruction":
+                ac = UIAlertController(title: "destruction", message: "mwahahaha", preferredStyle: .alert)
+                ac?.addAction(UIAlertAction(title: "...", style: .default))
+            case "remindLater":
+                ac = UIAlertController(title: "Scheduling notifcation", message: "10 sec from now so lock your screen", preferredStyle: .alert)
+                ac?.addAction(UIAlertAction(title: "got it", style: .default))
+                scheduleNotificatonInterval(title: "A custom notifcation", body: "You must have pressed 'remind later' in previous notificaton", identifier: "alarm")
+                
             default:
-                break
+                ac = UIAlertController(title: "huh", message: "what is going on...", preferredStyle: .alert)
+                ac?.addAction(UIAlertAction(title: "bruh", style: .default))
+            }
+            
+            if let ac = ac {
+                present(ac, animated: true)
             }
         }
         // must call completion handler

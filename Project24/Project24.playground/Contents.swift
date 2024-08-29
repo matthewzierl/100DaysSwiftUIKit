37902f686a6df5b021581c8704a1d6fb3f8dd269
matthewzierl <matthewtzierl@gmail.com>
2024-08-29 07:09:38 -0500

    Create splitByLines, isNumeric, withPrefix
    As per the 3 challenges, created withPrefix which
    adds a prefix to self if it doesn't exist. isNumeric
    checks for numerical values in self by calling
    double initiator. splitByLines returns an array
    of strings, each coming from an original strings
    separated by newlines. I achieved this by using
    calling firstIndexOf("\n") on a substring of self
    using self[currentIndex...].firstIndexOf and
    updating currentIndex each time a newline was
    found.

--- Project24/Project24.playground/Contents.swift
+++ Project24/Project24.playground/Contents.swift
@@ -8,3 +8,43 @@ attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        if self.hasPrefix(prefix) {
+            return self
+        } else {
+            return "\(self)\(prefix)"
+        }
+    }
+    
+    func isNumeric() -> Bool {
+        return Double(self) != nil
+    }
+    
+    func splitByLines() -> [String] {
+        if self.isEmpty { return [""] }
+        var currentIndex = self.startIndex
+        var arr = [String]()
+        
+        while let newLineIndex = self[currentIndex...].firstIndex(of: "\n") {
+            arr.append(String(self[currentIndex..<newLineIndex]))
+            currentIndex = self.index(after: newLineIndex)
+        }
+        if currentIndex < self.endIndex {
+            arr.append(String(self[currentIndex...]))
+        }
+        return arr
+    }
+}
+
+print(string.withPrefix("This"))
+print(string.isNumeric())
+
+let test = "this\nis\na\ntest"
+var arr = test.splitByLines()
+print(test)
+for sub in arr {
+    print(sub)
+}
